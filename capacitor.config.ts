923d1b0f1e4e078c0f63cbdfa5f442ee88e8589d
warodom2 <warodom@crmc.consulting>
2024-06-28 10:02:50 +0700

    Update app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'crm.crmc.crmctodo',
   appName: 'crmc-todo',
   webDir: 'www'
 };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addOutline, trashOutline } from 'ionicons/icons';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonIcon,
+    FormsModule,
+    CommonModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+  todos = signal<Todo[]>([]);
+  newTodo = signal('');
+  addOutline = addOutline;
+  trashOutline = trashOutline;
+
+  addTodo() {
+    if (this.newTodo()) {
+      this.todos.update((todos) => [
+        ...todos,
+        { id: Date.now(), text: this.newTodo(), completed: false },
+      ]);
+      this.newTodo.set('');
+    }
+  }
+
+  toggleTodo(id: number) {
+    this.todos.update((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  deleteTodo(id: number) {
+    this.todos.update((todos) => todos.filter((todo) => todo.id !== id));
+  }
 }
